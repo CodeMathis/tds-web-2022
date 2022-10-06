b0a40f1d2d10f659bc38381968b9163bb3f9c98a
Mathis Ravier <mravier@sio.local>
2022-10-06 16:01:55 +0200

    update php

--- php/variable.php
+++ php/variable.php
@@ -2,16 +2,18 @@
 $message = "Hello world!";
 $prenom = $_GET["prenom"]??"Anonyme";
 $max = $_GET["nb"]??5;
+$color = $_GET["color"]??"white";
+$bgcolor = $_GET["bgcolor"]??"black";
 
-echo "<h1>$message</h1>","<h2>$message</h2>" ,"<h3>$message</h3>" ,$message;
+echo "<h1>$message";
 echo "<br>Ton prenom est $prenom";
 
-foreach (range(0,10) as $number){
+foreach (range(0,5) as $number){
 	echo "<br>$max";
 }
 foreach (range(1,$max) as $number){
     if ($number > 6){
         echo "<br>";
     }
-	echo "<h$number>$prenom</h$number>";
+	echo "<h$number, style='color: $color;background-color: $bgcolor'>$prenom</h$number>";
 }

cbfee5a6c5db58ad72f7b8e0950781d719249c48
Mathis Ravier <mravier@sio.local>
2022-10-06 15:07:14 +0200

    update php

--- php/variable.php
+++ php/variable.php
@@ -1,2 +1,14 @@
 <?php
-echo "<h1>Hello world!</h1>";
+$message = "Hello world!";
+$prenom = $_GET["prenom"]??"Anonyme";
+$max = $_GET["nb"]??5;
+
+echo "<h1>$message</h1>","<h2>$message</h2>" ,"<h3>$message</h3>" ,$message;
+echo "<br>Ton prenom est $prenom";
+
+foreach (range(0,10) as $number){
+	echo "<br>$max";
+}
+foreach (range(1,$max) as $number){
+	echo "<h$number>$prenom</h$number>";
+}

edc94d46baf35e7c51e9d49d8d62c8a5994d04f9
Mathis Ravier <mravier@sio.local>
2022-12-01 17:06:55 +0100

    update

--- js/master-mind.js
+++ js/master-mind.js
@@ -4,7 +4,7 @@ for (i = 0; i < 12; i++) {
     affichage.innerHTML += "<div>";
     for (y = 0; y < 4; y++) {
         affichage.innerHTML += "<form style='display: inline-flex;'>\n" +
-            "    <select id=\"coulours"+i+"\" name=\"coulours\">\n" +
+            "    <select id=\"coulours" + y + "_" + i + "\" name=\"coulours\">\n" +
             "        <option value=\"red\" style=\"background: red;\">red</option>\n" +
             "        <option value=\"yellow\" style=\"background: yellow;\">yellow</option>\n" +
             "        <option value=\"blue\" style=\"background: blue;\">blue</option>\n" +
@@ -12,11 +12,11 @@ for (i = 0; i < 12; i++) {
             "        <option value=\"green\" style=\"background: green;\">green</option>\n" +
             "    </select>\n" +
             "</form>";
+        //document.getElementById("bouton_generer").addEventListener("click", function (){ });
     };
-    affichage.innerHTML += "  ";
-    for (y = 0; y < 4; y++) {
+    for (z = 0; z < 4; z++) {
         affichage.innerHTML += "<form style='display: inline-flex;'>\n" +
-            "    <select id=\"coulours_choice"+y+"\" name=\"coulours_choice\">\n" +
+            "    <select id=\"coulours_choice"+z+"_"+i+"\" name=\"coulours_choice\">\n" +
             "        <option value=\"white\" style=\"background: white;\">white</option>\n" +
             "        <option value=\"red\" style=\"background:red;\">red</option>\n" +
             "    </select>\n" +

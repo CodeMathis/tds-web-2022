e976493cadaf2a42e1ee85e46e0a041a7337d43f
met-meza <mathis.ravier@sts-sio-caen.info>
2022-12-03 11:33:52 +0100

    update

--- yaml/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content=file_get_contents('test.yaml');
-$yamlContent=yaml_parse($content);
-echo '<pre>'.print_r($yamlContent,true).'</pre>';
-?>
---
+++ yaml/test/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+$data = yaml_parse_file("test.yaml");
+echo "<p>".$data["lastname"]." ".$data["firstname"]."</p>";
+echo "<p>".$data["menu"][0]."</p>";
+*/
+
+
+
+?>

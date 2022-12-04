e6509e31b57c0de9a75e404496ae3d79bdef523d
met-meza <mathis.ravier@sts-sio-caen.info>
2022-12-04 23:02:42 +0100

    virer portfolio

--- portfolio/php/master.php
+++
@@ -1,9 +0,0 @@
-<?php
-$barmenu = yaml_parse_file('yaml/barmenu.yaml');
-$background1 = yaml_parse_file('yaml/background1.yaml');
-$background2 = yaml_parse_file('yaml/background2.yaml');
-$background3 = yaml_parse_file('yaml/background3.yaml');
-$background4 = yaml_parse_file('yaml/background4.yaml');
-$background5 = yaml_parse_file('yaml/background5.yaml');
-$background6 = yaml_parse_file('yaml/background6.yaml');
-?>

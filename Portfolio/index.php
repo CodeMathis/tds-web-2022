c625989726fbf51efd6e2573740ceda0b7b9f072
met-meza <mathis.ravier@sts-sio-caen.info>
2022-12-17 23:26:13 +0100

    minor update

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -19,11 +19,11 @@ include 'php/master.php';
 			<ul id="menu">
 				<?php
                 echo '<li><a href="#background1" id="titre_barmenu">'.$barmenu["titre"].'</a></li>';
-                echo '<span id="aligne_droite_barmenu">';
+                echo '<li id="aligne_droite_barmenu">';
                 foreach($barmenu["text_barmenu"] AS $index=>$titre){
                     echo '<li class="align_li"><a href="#background'.(1 + $index).'">'.$titre.'</a></li>';
                 }
-                echo '</span>';
+                echo '</li>';
                 ?>
 			</ul>
 			<hr>

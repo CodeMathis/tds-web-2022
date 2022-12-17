7171feb6a7c6894620c0d7774c97deb8c1d1ecfa
met-meza <mathis.ravier@sts-sio-caen.info>
2022-12-17 23:08:06 +0100

    minor update

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -134,7 +134,7 @@ include 'php/master.php';
                             <textarea class="cadre_input_et_textarea" required="required" name="message" rows="6" cols="40" placeholder="Vous pouvez saisir un message ici."></textarea>
                         </p>
                         <?php
-                        echo '<p>';
+                        echo '<p id="p_captcha">';
                         echo '<span class="g-recaptcha" data-sitekey="'.$secret_data["site_key"].'"></span>';
                         if (isset($_SESSION["formulaire_resultat"])){
                             echo $_SESSION["formulaire_resultat"];

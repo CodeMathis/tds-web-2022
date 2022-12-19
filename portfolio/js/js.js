cc0ab935e7488522807a40c680d1ca3c06442aea
met-meza <mathis.ravier@sts-sio-caen.info>
2022-12-19 23:10:17 +0100

    minor update

---
+++ portfolio/js/js.js
@@ -0,0 +1,22 @@
+//animations
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+
+        if (elementTop <= windowHeight - 110) {
+            reveals[i].classList.add("active");
+        } else if (elementTop >= windowHeight) {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("DOMContentLoaded", reveal);
+
+//ajuste hauteur body
+let header_height = document.getElementById("header_id").offsetHeight;
+console.log(header_height);
